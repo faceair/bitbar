cbeb8a97e18d3a201b09d0f90e237c18e8b73b9a
faceair <git@faceair.me>
2017-07-19 08:48:39 +0800

    show change & summary

--- eos-price.5s.py
+++ eos-price.5s.py
@@ -4,6 +4,8 @@
 import json
 import urllib2
 
+COIN_COUNT = 0
+
 
 def readPrice():
     file = open('/tmp/eos_price.txt', 'r')
@@ -27,16 +29,27 @@ def fetchPrice():
     }))
     body = json.loads(res.read())
     res.close()
-    return float(body['data'][0]['price_cny'])
+    return float(body['data'][0]['price_cny']), float(body['data'][0]['percent'])
 
 
 old_price = readPrice()
-price = fetchPrice()
+price, percent = fetchPrice()
 if price > old_price:
     print('↑%.2f | color=green' % price)
 elif price == old_price:
     print("%.2f" % price)
 elif price < old_price:
     print('↓%.2f | color=red' % price)
+
+print('---')
+
+if percent > 0:
+    print('Change: +%.2f%% | color=green' % percent)
+elif percent == 0:
+    print('Change: %.2f%%' % percent)
+elif percent < 0:
+    print('Change: %.2f%% | color=red' % percent)
+print("Summary: ￥%.2f | color=black" % (price * COIN_COUNT))
+
 
 writePrice(price)
