305823ea7f5e2174abf5ffa89f791354ce0c55b2
faceair <git@faceair.me>
2018-01-22 14:07:02 +0800

    add proxy switch

--- auto-proxy.5s.bash
+++ auto-proxy.5s.bash
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-if [ -n "$(networksetup -getairportnetwork en0 | cut -c 24- | grep faceairAP)" ] ; then
-  networksetup -setsocksfirewallproxystate Wi-Fi off
-  printf "" > ~/.config/fish/conf.d/proxy.fish
-else
+if [ ! -f /tmp/disable_bitbar_auto_proxy ]; then
   networksetup -setsocksfirewallproxy "Wi-Fi" 127.0.0.1 1080
   networksetup -setsocksfirewallproxystate Wi-Fi on
-  printf "set -gx http_proxy socks5://127.0.0.1:1080\nset -gx https_proxy socks5://127.0.0.1:1080" > ~/.config/fish/conf.d/proxy.fish
-  printf "✈️"
+  echo -e "set -gx http_proxy socks5://127.0.0.1:1080\nset -gx https_proxy socks5://127.0.0.1:1080" > ~/.config/fish/conf.d/proxy.fish
+  echo "✈️| bash=/usr/bin/touch param1=/tmp/disable_bitbar_auto_proxy terminal=false"
+else
+  networksetup -setsocksfirewallproxystate Wi-Fi off
+  echo "" > ~/.config/fish/conf.d/proxy.fish
+  echo "❕| bash=/bin/rm param1=/tmp/disable_bitbar_auto_proxy terminal=false"
 fi
